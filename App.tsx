20c6aa536729242578647276097ae2513b346658
unknown <helio.paraiso@gmail.com>
2020-09-27 13:30:08 -0300

    Primeiro commit 1.1

--- App.tsx
+++ App.tsx
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import Header from './src/Components/HeaderComponent/Header';
+import { StyleSheet,View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapStyle from './src/Components/MapComponents/MapStyle'
 export default function App() {
+
+  const [Currentlocation, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('A persmissão de acesso foi negada!')
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      setLocation({
+        latitude: latitude,
+        longitude: longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+
+      })
+      console.log(Currentlocation)
+
+    }
+    getCurrentLocation();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true} />
+      <MapView
+        style={styles.mapStyle}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={Currentlocation}
+        showsUserLocation={true}
+        showsBuildings={false}
+        showsPointsOfInterest={false}
+        showsTraffic={false}
+        showsIndoors={false}
+        customMapStyle={MapStyle}
+        >
+        <Header/>
+      </MapView>
     </View>
   );
 }
@@ -18,4 +59,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
---
+++ src/Components/HeaderComponent/HeaderStyle.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+display: flex;
+flex-direction:row;
+justify-content:space-between;
+align-content:center;
+margin:10px;
+
+`;
+
+
+export const AppName = styled.Text`
+color: white;
+margin-top:4px;
+
+
+`;
+
+export const Avatar = styled.Image`
+  border: 2px solid #8E2DE2;
+  width:45px;
+  height:45px;
+   border-radius:24px;
+`;
